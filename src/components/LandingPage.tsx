27a48f003ad545361aa4ae5a7c771cbb057872cc
cyanideph <67945955+cyanideph@users.noreply.github.com>
2024-09-04 21:50:48 +0800

    Update LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -31,7 +31,7 @@ export default function LandingPage() {
         <div className="absolute inset-0">
           <div className="relative w-full h-full">
             <Image
-              src="/images/img.jpg"
+              src="/images/img.png"
               alt="Construction site"
               fill
               style={{ objectFit: 'cover' }}
