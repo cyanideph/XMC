483556366bea851ebd6efd8c914f66bef54af577
cyanideph <67945955+cyanideph@users.noreply.github.com>
2024-09-04 11:02:56 +0800

    Create useParallax.ts

---
+++ src/hooks/useParallax.ts
@@ -0,0 +1,9 @@
+import { useTransform, useScroll, MotionValue } from 'framer-motion'
+import { useRef } from 'react'
+
+export function useParallax(distance: number = 300): [React.RefObject<HTMLDivElement>, MotionValue<string>] {
+  const ref = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({ target: ref })
+  const y = useTransform(scrollYProgress, [0, 1], ['0%', `${distance}%`])
+  return [ref, y]
+}
