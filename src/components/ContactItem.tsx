139d36423bd1c8fbde2346c819967448413e1947
cyanideph <67945955+cyanideph@users.noreply.github.com>
2024-09-05 16:53:14 +0800

    Create ContactItem.tsx

---
+++ src/components/ContactItem.tsx
@@ -0,0 +1,16 @@
+import { motion } from 'framer-motion';
+
+export function ContactItem({
+  icon,
+  text,
+}: {
+  icon: React.ReactNode;
+  text: string;
+}) {
+  return (
+    <motion.li className="flex items-center space-x-4" variants={slideInLeft}>
+      <div className="text-red-500">{icon}</div>
+      <p className="text-lg text-white">{text}</p>
+    </motion.li>
+  );
+}
